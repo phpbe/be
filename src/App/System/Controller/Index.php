642cb979842177e2700b7e77c5fb6dbab7751dc8
liu12 <i@liu12.com>
2022-02-25 17:34:41 +0800

    移除测试代码

--- src/App/System/Controller/Index.php
+++ src/App/System/Controller/Index.php
@@ -3,19 +3,12 @@
 namespace Be\App\System\Controller;
 
 use Be\Be;
-use Be\Util\Str\Pinyin;
 
 class Index
 {
 
     public function index()
     {
-        $str = 'aa中国人rrr ss  e a 中 国';
-        echo $str;
-        echo '<br/>';
-        //$str = str_replace(' ', '-', $str);
-        echo Pinyin::convert($str, '-', false);
-        exit;
         Be::getResponse()->display();
     }
 
