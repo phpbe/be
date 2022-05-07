64af9d5f0b45645b71c08e9fe229765893050976
liu12 <i@liu12.com>
2022-05-07 15:16:45 +0800

    手工启动计划任务

--- src/AdminPlugin/Task/Task.php
+++ src/AdminPlugin/Task/Task.php
@@ -424,7 +424,7 @@ class Task extends Driver
             $postData = $request->json();
             $tupleTask = Be::getTuple('system_task');
             $tupleTask->load($postData['row']['id']);
-            Be::getService('App.System.Task')->trigger($tupleTask->app . '.' . $tupleTask->name, 'MANUAL');
+            Be::getService('App.System.Task')->trigger($tupleTask->app . '.' . $tupleTask->name, null, 'MANUAL');
             beAdminOpLog('手工启动任务：' . $tupleTask->label . '（' . $tupleTask->app . '.' . $tupleTask->name . '）');
             $response->success('任务启动成功！');
         } catch (\Throwable $t) {
