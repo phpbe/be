1cfa4aea82bb12f0812b989c0fd0570c751ef917
liu12 <i@liu12.com>
2022-07-15 11:23:14 +0800

    允许上传 ttf woff 字体文件

--- src/App/System/Config/System.php
+++ src/App/System/Config/System.php
@@ -28,7 +28,7 @@ class System
     /**
      * @BeConfigItem("允许上传的文件类型", driver="FormItemCode", language="json", valueType = "array(string)")
      */
-    public $allowUploadFileTypes = ['jpg', 'jpeg', 'gif', 'png', 'svg', 'webp', 'txt', 'pdf', 'doc', 'docx', 'csv', 'xls', 'xlsx', 'ppt', 'pptx', 'zip', 'rar'];
+    public $allowUploadFileTypes = ['jpg', 'jpeg', 'gif', 'png', 'svg', 'webp', 'txt', 'pdf', 'doc', 'docx', 'csv', 'xls', 'xlsx', 'ppt', 'pptx', 'zip', 'rar', 'ttf', 'woff'];
 
     /**
      * @BeConfigItem("允许上传的图片类型", driver="FormItemCode", language="json", valueType = "array(string)")
