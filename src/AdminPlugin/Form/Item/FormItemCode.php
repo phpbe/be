ffb759e635b5fc5672be87d2b60b80b89d030243
Lou Barnes <8981024@qq.com>
2023-03-04 17:52:21 +0800

    FormItemCode 非必填

--- src/AdminPlugin/Form/Item/FormItemCode.php
+++ src/AdminPlugin/Form/Item/FormItemCode.php
@@ -32,249 +32,217 @@ class FormItemCode extends FormItem
             }
         }
 
-        if (isset($params['language'])) {
-            $language = $params['language'];
-            if ($language instanceof \Closure) {
-                $language = $language($row);
+        if (!isset($params['language'])) {
+            $params['language'] = 'auto';
+        }
+
+        $language = $params['language'];
+        if ($language instanceof \Closure) {
+            $language = $language($row);
+        }
+
+        $baseUrl = Be::getProperty('App.System')->getWwwUrl() . '/lib/codemirror/codemirror-5.57.0/';
+
+        switch ($language) {
+            case 'html':
+                $this->js = [
+                    $baseUrl . 'lib/codemirror.js',
+                    $baseUrl . 'mode/htmlmixed/htmlmixed.js',
+                    $baseUrl . 'mode/xml/xml.js',
+                    $baseUrl . 'mode/javascript/javascript.js',
+                    $baseUrl . 'mode/css/css.js',
+                ];
+
+                $this->css = [
+                    $baseUrl . 'lib/codemirror.css',
+                ];
+
+                $this->options = [
+                    'theme' => 'default',
+                    'mode' => 'htmlmixed',
+                    'lineNumbers' => true,
+                    'lineWrapping' => true,
+                ];
+
+                break;
+            case 'css':
+                $this->js = [
+                    $baseUrl . 'lib/codemirror.js',
+                    $baseUrl . 'mode/css/css.js',
+                    $baseUrl . 'addon/edit/matchbrackets.js',
+                    $baseUrl . 'addon/hint/show-hint.js',
+                    $baseUrl . 'addon/hint/css-hint.js',
+                    $baseUrl . 'addon/css/css.js',
+                ];
+
+                $this->css = [
+                    $baseUrl . 'lib/codemirror.css',
+                    $baseUrl . 'addon/hint/show-hint.css',
+                ];
+
+                $this->options = [
+                    'theme' => 'default',
+                    'mode' => 'text/css',
+                    'lineNumbers' => true,
+                    'lineWrapping' => true,
+                    'matchBrackets' => true,
+                ];
+                break;
+            case 'javascript':
+                $this->js = [
+                    $baseUrl . 'lib/codemirror.js',
+                    $baseUrl . 'mode/javascript/javascript.js',
+                    $baseUrl . 'addon/edit/matchbrackets.js',
+                ];
+
+                $this->css = [
+                    $baseUrl . 'lib/codemirror.css',
+                ];
+
+                $this->options = [
+                    'theme' => 'default',
+                    'mode' => 'text/javascript',
+                    'lineNumbers' => true,
+                    'lineWrapping' => true,
+                    'matchBrackets' => true,
+                ];
+
+                break;
+            case 'json':
+                $this->js = [
+                    $baseUrl . 'lib/codemirror.js',
+                    $baseUrl . 'mode/javascript/javascript.js',
+                    $baseUrl . 'addon/edit/matchbrackets.js',
+                    $baseUrl . 'addon/lint/jsonlint-1.6.0.js',
+                    $baseUrl . 'addon/lint/lint.js',
+                    $baseUrl . 'addon/lint/json-lint.js',
+
+                ];
+
+                $this->css = [
+                    $baseUrl . 'lib/codemirror.css',
+                    $baseUrl . 'addon/lint/lint.css',
+                ];
+
+                $this->options = [
+                    'theme' => 'default',
+                    'mode' => 'application/json',
+                    'lineNumbers' => true,
+                    'lineWrapping' => true,
+                    'gutters' => ['CodeMirror-lint-markers'],
+                    'matchBrackets' => true,
+                    'lint' => true,
+                ];
+                break;
+            case 'php':
+                $this->js = [
+                    $baseUrl . 'lib/codemirror.js',
+                    $baseUrl . 'mode/clike/clike.js',
+                    $baseUrl . 'mode/php/php.js',
+                    $baseUrl . 'addon/edit/matchbrackets.js',
+                ];
+
+                $this->css = [
+                    $baseUrl . 'lib/codemirror.css',
+                ];
+
+                $this->options = [
+                    'theme' => 'default',
+                    'mode' => 'text/x-php',
+                    'lineNumbers' => true,
+                    'lineWrapping' => true,
+                    'matchBrackets' => true,
+                ];
+                break;
+            case 'sql':
+                $this->js = [
+                    $baseUrl . 'lib/codemirror.js',
+                    $baseUrl . 'mode/sql/sql.js',
+                    $baseUrl . 'addon/edit/matchbrackets.js',
+                    $baseUrl . 'addon/hint/show-hint.js',
+                    $baseUrl . 'addon/hint/sql-hint.js',
+                ];
+
+                $this->css = [
+                    $baseUrl . 'lib/codemirror.css',
+                    $baseUrl . 'addon/hint/show-hint.css',
+                ];
+
+                $this->options = [
+                    'theme' => 'default',
+                    'mode' => 'text/x-sql',
+                    'lineNumbers' => true,
+                    'lineWrapping' => true,
+                    'matchBrackets' => true,
+                ];
+
+                break;
+            case 'xml':
+                $this->js = [
+                    $baseUrl . 'lib/codemirror.js',
+                    $baseUrl . 'mode/xml/xml.js',
+                ];
+
+                $this->css = [
+                    $baseUrl . 'lib/codemirror.css',
+                ];
+
+                $this->options = [
+                    'theme' => 'default',
+                    'mode' => 'text/html',
+                    'lineNumbers' => true,
+                    'lineWrapping' => true,
+                ];
+                break;
+
+            default:
+                $this->js = [
+                    $baseUrl . 'lib/codemirror.js',
+                    $baseUrl . 'mode/javascript/javascript.js',
+                ];
+
+                $this->css = [
+                    $baseUrl . 'lib/codemirror.css',
+                ];
+
+                $this->options = [
+                    'theme' => 'default',
+                    'mode' => 'javascript',
+                    'lineNumbers' => true,
+                    'lineWrapping' => true,
+                ];
+        }
+
+        if (isset($params['js'])) {
+            $js = $params['js'];
+            if ($js instanceof \Closure) {
+                $js = $js($row);
             }
 
-            $baseUrl = Be::getProperty('App.System')->getWwwUrl() . '/lib/codemirror/codemirror-5.57.0/';
+            if (is_array($js)) {
+                $this->js = array_merge($this->js, $js);
+            }
+        }
 
-            switch ($language) {
-                case 'html':
-                    $this->js = [
-                        $baseUrl . 'lib/codemirror.js',
-                        $baseUrl . 'mode/htmlmixed/htmlmixed.js',
-                        $baseUrl . 'mode/xml/xml.js',
-                        $baseUrl . 'mode/javascript/javascript.js',
-                        $baseUrl . 'mode/css/css.js',
-                    ];
-
-                    $this->css = [
-                        $baseUrl . 'lib/codemirror.css',
-                    ];
-
-                    $this->options = [
-                        'theme' => 'default',
-                        'mode' => 'htmlmixed',
-                        'lineNumbers' => true,
-                        'lineWrapping' => true,
-                    ];
-
-                    break;
-                case 'css':
-                    $this->js = [
-                        $baseUrl . 'lib/codemirror.js',
-                        $baseUrl . 'mode/css/css.js',
-                        $baseUrl . 'addon/edit/matchbrackets.js',
-                        $baseUrl . 'addon/hint/show-hint.js',
-                        $baseUrl . 'addon/hint/css-hint.js',
-                        $baseUrl . 'addon/css/css.js',
-                    ];
-
-                    $this->css = [
-                        $baseUrl . 'lib/codemirror.css',
-                        $baseUrl . 'addon/hint/show-hint.css',
-                    ];
-
-                    $this->options = [
-                        'theme' => 'default',
-                        'mode' => 'text/css',
-                        'lineNumbers' => true,
-                        'lineWrapping' => true,
-                        'matchBrackets' => true,
-                    ];
-                    break;
-                case 'javascript':
-                    $this->js = [
-                        $baseUrl . 'lib/codemirror.js',
-                        $baseUrl . 'mode/javascript/javascript.js',
-                        $baseUrl . 'addon/edit/matchbrackets.js',
-                    ];
-
-                    $this->css = [
-                        $baseUrl . 'lib/codemirror.css',
-                    ];
-
-                    $this->options = [
-                        'theme' => 'default',
-                        'mode' => 'text/javascript',
-                        'lineNumbers' => true,
-                        'lineWrapping' => true,
-                        'matchBrackets' => true,
-                    ];
-
-                    break;
-                case 'json':
-                    $this->js = [
-                        $baseUrl . 'lib/codemirror.js',
-                        $baseUrl . 'mode/javascript/javascript.js',
-                        $baseUrl . 'addon/edit/matchbrackets.js',
-                        $baseUrl . 'addon/lint/jsonlint-1.6.0.js',
-                        $baseUrl . 'addon/lint/lint.js',
-                        $baseUrl . 'addon/lint/json-lint.js',
-
-                    ];
-
-                    $this->css = [
-                        $baseUrl . 'lib/codemirror.css',
-                        $baseUrl . 'addon/lint/lint.css',
-                    ];
-
-                    $this->options = [
-                        'theme' => 'default',
-                        'mode' => 'application/json',
-                        'lineNumbers' => true,
-                        'lineWrapping' => true,
-                        'gutters' => ['CodeMirror-lint-markers'],
-                        'matchBrackets' => true,
-                        'lint' => true,
-                    ];
-                    break;
-                case 'php':
-                    $this->js = [
-                        $baseUrl . 'lib/codemirror.js',
-                        $baseUrl . 'mode/clike/clike.js',
-                        $baseUrl . 'mode/php/php.js',
-                        $baseUrl . 'addon/edit/matchbrackets.js',
-                    ];
-
-                    $this->css = [
-                        $baseUrl . 'lib/codemirror.css',
-                    ];
-
-                    $this->options = [
-                        'theme' => 'default',
-                        'mode' => 'text/x-php',
-                        'lineNumbers' => true,
-                        'lineWrapping' => true,
-                        'matchBrackets' => true,
-                    ];
-                    break;
-                case 'sql':
-                    $this->js = [
-                        $baseUrl . 'lib/codemirror.js',
-                        $baseUrl . 'mode/sql/sql.js',
-                        $baseUrl . 'addon/edit/matchbrackets.js',
-                        $baseUrl . 'addon/hint/show-hint.js',
-                        $baseUrl . 'addon/hint/sql-hint.js',
-                    ];
-
-                    $this->css = [
-                        $baseUrl . 'lib/codemirror.css',
-                        $baseUrl . 'addon/hint/show-hint.css',
-                    ];
-
-                    $this->options = [
-                        'theme' => 'default',
-                        'mode' => 'text/x-sql',
-                        'lineNumbers' => true,
-                        'lineWrapping' => true,
-                        'matchBrackets' => true,
-                    ];
-
-                    break;
-                case 'xml':
-                    $this->js = [
-                        $baseUrl . 'lib/codemirror.js',
-                        $baseUrl . 'mode/xml/xml.js',
-                    ];
-
-                    $this->css = [
-                        $baseUrl . 'lib/codemirror.css',
-                    ];
-
-                    $this->options = [
-                        'theme' => 'default',
-                        'mode' => 'text/html',
-                        'lineNumbers' => true,
-                        'lineWrapping' => true,
-                    ];
-                    break;
-
-                default:
-                    $this->js = [
-                        $baseUrl . 'lib/codemirror.js',
-                        $baseUrl . 'mode/javascript/javascript.js',
-                    ];
-
-                    $this->css = [
-                        $baseUrl . 'lib/codemirror.css',
-                    ];
-
-                    $this->options = [
-                        'theme' => 'default',
-                        'mode' => 'javascript',
-                        'lineNumbers' => true,
-                        'lineWrapping' => true,
-                    ];
+        if (isset($params['css'])) {
+            $css = $params['css'];
+            if ($css instanceof \Closure) {
+                $css = $css($row);
             }
 
-            if (isset($params['js'])) {
-                $js = $params['js'];
-                if ($js instanceof \Closure) {
-                    $js = $js($row);
-                }
+            if (is_array($css)) {
+                $this->css = array_merge($this->css, $css);
+            }
+        }
 
-                if (is_array($js)) {
-                    $this->js = array_merge($this->js, $js);
-                }
+        if (isset($params['options'])) {
+            $options = $params['options'];
+            if ($options instanceof \Closure) {
+                $options = $options($row);
             }
 
-            if (isset($params['css'])) {
-                $css = $params['css'];
-                if ($css instanceof \Closure) {
-                    $css = $css($row);
-                }
-
-                if (is_array($css)) {
-                    $this->css = array_merge($this->css, $css);
-                }
-            }
-
-            if (isset($params['options'])) {
-                $options = $params['options'];
-                if ($options instanceof \Closure) {
-                    $options = $options($row);
-                }
-
-                if (is_array($options)) {
-                    $this->options = array_merge($this->options, $options);
-                }
-            }
-
-        } else {
-
-            if (isset($params['js'])) {
-                $js = $params['js'];
-                if ($js instanceof \Closure) {
-                    $js = $js($row);
-                }
-                if (is_array($js)) {
-                    $this->js = $js;
-                }
-            }
-
-            if (isset($params['css'])) {
-                $css = $params['css'];
-                if ($css instanceof \Closure) {
-                    $css = $css($row);
-                }
-
-                if (is_array($css)) {
-                    $this->css = $css;
-                }
-            }
-
-            if (isset($params['options'])) {
-                $options = $params['options'];
-                if ($options instanceof \Closure) {
-                    $options = $options($row);
-                }
-
-                if (is_array($options)) {
-                    $this->options = $options;
-                }
+            if (is_array($options)) {
+                $this->options = array_merge($this->options, $options);
             }
         }
     }
