5dd29da55c309f3ad37db009c67e96a464b72f9f
刘国强 <i@liu12.com>
2024-10-08 09:14:25 +0800

    docker compose nginx 启动命 令

--- docker-compose/nginx/entrypoint.sh
+++ docker-compose/nginx/entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 if [ "${NGINX_HTTPS_ENABLED}" = "true" ]; then
-    # SSL 证书
-    HTTPS_CONFIG=$(envsubst < /etc/nginx/https.conf.template)
-    export HTTPS_CONFIG
+  # SSL 证书
+  HTTPS_CONFIG=$(envsubst < /etc/nginx/https.conf.template)
+else
+  HTTPS_CONFIG=''
 fi
+
+export HTTPS_CONFIG
 
 env_vars=$(printenv | cut -d= -f1 | sed 's/^/$/g' | paste -sd, -)
 
-envsubst < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst "$env_vars" < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
