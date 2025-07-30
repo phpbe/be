8ef8ee4f9dde5740551e9d4ce372ed864afba1a6
liu12 <8981024@qq.com>
2025-07-30 22:06:29 +0800

    构建优化

--- docker/apache/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-version=7.4.30-apache-buster
-docker build --build-arg PHP_VERSION=${version} -t phpbe/be:${version}  .
-docker push phpbe/be:${version}
-
-docker tag phpbe/be:${version} phpbe/be:latest
-docker push phpbe/be:latest
